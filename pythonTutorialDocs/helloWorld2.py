1c973aae3126371e6e0c87fd8928b4a4259f4682
ronald1195 <ronald1195@icloud.com>
2019-10-19 13:13:07 -0600

    Adding py files

---
+++ pythonTutorialDocs/helloWorld2.py
@@ -0,0 +1,7 @@
+print '\n\t\tHello there!'
+print '\t\tWhat is yout name?\n'
+
+inputValue = raw_input("\tInsert your name here: ")
+
+
+print "\n\t\tHello " + inputValue + "!\n\t\tWELCOME TO PYTHON\n"
